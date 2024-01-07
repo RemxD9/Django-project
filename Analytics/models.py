f3051dbc221f7a8ef69e77fcd0ebd2e33ca04a9d
RemxD9 <vladislav.iatsuk@urfu.me>
2024-01-07 17:00:13 +0500

    Добавлены заготовки для таблиц и графиков, верстка и стили. Также добавил получение последних вакансий по апи ххру

--- Analytics/models.py
+++ Analytics/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    skills = models.TextField()
+    company = models.CharField(max_length=255)
+    salary = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    publication_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- Analytics/views.py
+++ Analytics/views.py
@@ -1,21 +1,89 @@
+from datetime import datetime
+
+import requests
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Vacancy
 
 
 def main_page(request):
-    return render(request, 'index.html')
+    return render(request, 'mainpage.html')
 
 
 def popularity(request):
-    return render(request, 'index.html')
+    return render(request, 'popularity.html')
 
 
 def geography(request):
-    return render(request, 'index.html')
+    return render(request, 'geography.html')
 
 
 def skills(request):
-    return render(request, 'index.html')
+    return render(request, 'skills.html')
+
+
+def get_hh_vacancy_description(vacancy_id):
+    url = f'https://api.hh.ru/vacancies/{vacancy_id}'
+    response = requests.get(url)
+    data = response.json()
+    return data.get('description', '')
+
+
+def get_hh_vacancy_skills(vacancy_id):
+    url = f'https://api.hh.ru/vacancies/{vacancy_id}/key_skills'
+    response = requests.get(url)
+    data = response.json()
+    skills = ', '.join(skill.get('name', '') for skill in data)
+    return skills
+
+
+def get_hh_vacancies(profession):
+    url = f'https://api.hh.ru/vacancies'
+    params = {
+        'text': profession,
+        'area': 1,
+        'period': 1,
+        'per_page': 10,
+        'order_by': 'published_at',
+    }
+    response = requests.get(url, params=params)
+    data = response.json().get('items', [])
+
+    vacancies = []
+    for item in data:
+        vacancy_id = item.get('id', '')
+        description = get_hh_vacancy_description(vacancy_id)
+        skills = get_hh_vacancy_skills(vacancy_id)
+
+        vacancy = {
+            'title': item.get('name', ''),
+            'description': description,
+            'skills': skills,
+            'company': item.get('employer', {}).get('name', ''),
+            'salary': item.get('salary', {}).get('from', ''),
+            'region': item.get('area', {}).get('name', ''),
+            'publication_date': datetime.fromisoformat(item.get('published_at')).replace(tzinfo=timezone.utc),
+        }
+        vacancies.append(vacancy)
+
+    return vacancies
+
+
+def get_hh_vacancies_for_multiple_professions(professions):
+    all_vacancies = []
+    for profession in professions:
+        vacancies = get_hh_vacancies(profession)
+        all_vacancies.extend(vacancies)
+    return all_vacancies
 
 
 def last_vacancies(request):
-    return render(request, 'index.html')
+    professions = ['devops', 'development operations']
+    vacancies = get_hh_vacancies_for_multiple_professions(professions)
+
+    for vacancy_data in vacancies:
+        Vacancy.objects.create(**vacancy_data)
+
+    context = {'vacancies': Vacancy.objects.all()}
+    return render(request, 'last_vacancies.html', context)
